aabdc8b9d062cc00a13edd3db2faec5474b112fe
zhangbin <zhangbin@cocos2d-x.org>
2016-05-04 13:58:46 +0800

    Add logic for init base properties & import Sprite.

--- core/ccb-importer.js
+++ core/ccb-importer.js
@@ -6,12 +6,15 @@ const Fs = require('fire-fs');
 const Plist = require('plist');
 const Url = require('fire-url');
 
+const DEFAULT_SP_URL = 'db://internal/image/default_sprite.png/default_sprite';
+
 const nodeCreators = {
 
 };
 
 const nodeImporters = {
-    'CCSprite' : _initSprite
+    'CCSprite' : _initSprite,
+    'CCScale9Sprite' : _initScale9Sprite
 };
 
 var resRootUrl = '';
@@ -171,6 +174,7 @@ function _createNodeGraph(rootNode, nodeData, curNodePath, cb) {
                         newNode.setName(theNodeName);
                         addedChildNames.push(theNodeName);
                         rootNode.addChild(newNode);
+                        newNode.setPosition(_convertNodePos(newNode));
                         index++;
                         callback();
                     });
@@ -190,8 +194,7 @@ function _genChildName(nodeData, addedChildNames) {
     var retName = nameInData.replace('/', '_');
     var i = 1;
     while (addedChildNames.indexOf(retName) >= 0) {
-        retName += '_';
-        retName += i;
+        retName = (nameInData + '_' + i);
         i++;
     }
 
@@ -202,10 +205,33 @@ function _genChildName(nodeData, addedChildNames) {
     return retName;
 }
 
+function _genProperties(nodeData) {
+    var properties = nodeData.properties;
+    var ret = {};
+    for (var i = 0, n = properties.length; i < n; i++) {
+        var propInfo = properties[i];
+        ret[propInfo.name] = {
+            type : propInfo.type,
+            value : propInfo.value
+        }
+    }
+
+    return ret;
+}
+
+function _getProperty(props, key, defaultValue) {
+    if (props[key]) {
+        return props[key].value;
+    }
+
+    return defaultValue;
+}
+
 function _initNode(node, nodeData, cb) {
     var nodeType = nodeData.baseClass;
 
-    _initBaseProperties(node, nodeData);
+    var props = _genProperties(nodeData);
+    _initBaseProperties(node, props);
 
     node.active = true;
 
@@ -214,18 +240,157 @@ function _initNode(node, nodeData, cb) {
 
     // init the node with data of specified type
     if (nodeType && nodeImporters[nodeType]) {
-        nodeImporters[nodeType](node, nodeData, cb);
+        nodeImporters[nodeType](node, props, cb);
     } else {
         cb();
     }
 }
 
-function _initBaseProperties(node, nodeData) {
+function _convertNodePos(node, curPos) {
+    if (!curPos) {
+        curPos = node.getPosition();
+    }
 
+    var parent = node.getParent();
+    if (!parent) {
+        return curPos;
+    }
+
+    var parentAnchor = parent.getAnchorPoint();
+    var parentSize = parent.getContentSize();
+    var newX = curPos.x - parentSize.width * parentAnchor.x;
+    var newY = curPos.y - parentSize.height * parentAnchor.y;
+    return cc.p(newX, newY);
 }
 
-function _initSprite(node, nodeData, cb) {
+function _initBaseProperties(node, props) {
+    var anchorValue = _getProperty(props, 'anchorPoint', [ 0.5, 0.5]);
+    var ignoreAnchor = _getProperty(props, 'ignoreAnchorPointForPosition', false);
+    if (ignoreAnchor) {
+        node.setAnchorPoint(0, 0);
+    } else {
+        node.setAnchorPoint(anchorValue[0], anchorValue[1]);
+    }
+    var sizeValue = _getProperty(props, 'contentSize', [ 0.0, 0.0, 0 ]);
+    node.setContentSize(sizeValue[0], sizeValue[1]);
+    var posValue = _getProperty(props, 'position', [ 0.0, 0.0, 0 ]);
+    node.setPosition(posValue[0], posValue[1]);
+    node.setRotation(_getProperty(props, 'rotation', 0));
+
+    // init the scale value
+    var flipValue = _getProperty(props, 'flip', [ false, false ]);
+    var scaleValue = _getProperty(props, 'scale', [ 1.0, 1.0, false, 0 ]);
+    var scaleX = flipValue[0] ? (scaleValue[0] * -1) : scaleValue[0];
+    var scaleY = flipValue[1] ? (scaleValue[1] * -1) : scaleValue[1];
+    node.setScale(scaleX, scaleY);
+
+    // init the node color
+    var colorValue = _getProperty(props, 'color', [ 255, 255, 255 ]);
+    node.setColor(new cc.Color(colorValue[0], colorValue[1], colorValue[2]));
+    var opacityValue = _getProperty(props, 'opacity', 255);
+    node.setOpacity(opacityValue);
+}
+
+function _getSpriteFrame(frameData, defaultUrl) {
+    var retUrl = '';
+    if (!frameData || frameData.length < 2 ||
+        (!frameData[0] && !frameData[1])) {
+        // using default image
+        retUrl = defaultUrl;
+    } else {
+        if (frameData[0]) {
+            var plistUrl = Url.join(resRootUrl, frameData[0]);
+            retUrl = Url.join(plistUrl, frameData[1]);
+        } else {
+            retUrl = Url.join(resRootUrl, frameData[1]);
+            retUrl = Url.join(retUrl, Url.basenameNoExt(retUrl));
+        }
+    }
+
+    if (retUrl) {
+        var uuid = Editor.assetdb.remote.urlToUuid(retUrl);
+        if (Editor.assetdb.remote.existsByUuid(uuid)) {
+            var frame = new cc.SpriteFrame();
+            frame._uuid = uuid;
+            return frame;
+        }
+    }
+
+    return null;
+}
+
+function _initSprite(node, props, cb) {
+    _initSpriteWithSizeMode(node, props, cc.Sprite.SizeMode.RAW);
     cb();
+}
+
+function _initSpriteWithSizeMode(node, props, sizeMode) {
+    var sp = node.addComponent(cc.Sprite);
+    if (!sp) {
+        return;
+    }
+
+    // init blend function
+    var blendValue = _getProperty(props, 'blendFunc', [ 770, 771 ]);
+    sp.srcBlendFactor = (blendValue[0] === 1 ? 770 : blendValue[0]);
+    sp.dstBlendFactor = blendValue[1];
+
+    // init file data
+    var frameData = _getProperty(props, 'displayFrame', null);
+    sp.sizeMode = sizeMode;
+    sp.trim = false;
+    sp.spriteFrame = _getSpriteFrame(frameData, DEFAULT_SP_URL);
+}
+
+function _setScale9Properties(props, uuid, cb) {
+    Editor.assetdb.queryMetaInfoByUuid(uuid, function(err,info) {
+        if (!info) {
+            cb();
+            return;
+        }
+
+        // modify the meta info
+        var meta = JSON.parse(info.json);
+
+        meta.trimThreshold = -1;
+        meta.borderTop = _getProperty(props, 'insetTop', 0);
+        meta.borderBottom = _getProperty(props, 'insetBottom', 0);
+        meta.borderLeft = _getProperty(props, 'insetLeft', 0);
+        meta.borderRight = _getProperty(props, 'insetRight', 0);
+
+        var jsonString = JSON.stringify(meta);
+        Editor.assetdb.saveMeta( uuid, jsonString );
+        cb();
+    });
+}
+
+function _initScale9Sprite(node, props, cb) {
+    Async.waterfall([
+        function(next) {
+            _initSpriteWithSizeMode(node, props, cc.Sprite.SizeMode.CUSTOM);
+            next();
+        },
+        function(next) {
+            var sp = node.getComponent(cc.Sprite);
+            if (!sp) {
+                next();
+                return;
+            }
+
+            // refresh the contentSize of node
+            var curSize = node.getContentSize();
+            var preferedSize = _getProperty(props, 'preferedSize', [ curSize.width, curSize.height, 0 ]);
+            node.setContentSize(preferedSize[0], preferedSize[1]);
+
+            if (sp.spriteFrame) {
+                sp.type = cc.Sprite.Type.SLICED;
+                var uuid = sp.spriteFrame._uuid;
+                _setScale9Properties(props, uuid, next);
+            } else {
+                next();
+            }
+        }
+    ], cb);
 }
 
 module.exports = {
